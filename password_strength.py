31493e1b2218ec13f96b78e9c3f9eaa8507d7078
Alt13 <Alt90@rambler.ru>
2016-10-01 13:28:34 +0300

    Добавил функцию поиска спецсимволов в строке

--- password_strength.py
+++ password_strength.py
@@ -4,25 +4,32 @@ import string
 from random import choice
 
 
-bad_password = ['123456',
-                'password',
-                '12345678',
-                'qwerty',
-                '12345',
-                '123456789',
-                'football',
-                '1234',
-                '1234567',
-                'baseball',
-                'welcome', ]
+password_blacklist = ['123456',
+                      'password',
+                      '12345678',
+                      'qwerty',
+                      '12345',
+                      '123456789',
+                      'football',
+                      '1234',
+                      '1234567',
+                      'baseball',
+                      'welcome', ]
 
 personal_information = ['BMSTUfn11',
                         'fn11BMSTU07',
                         'BMSTUfn11199013', ]
 
 
+def if_not_simbol_in_password(password, simbpl_list):
+    for simbol in simbpl_list:
+        if (simbol in password):
+            return False
+    return True
+
+
 def get_password_strength(password):
-    if (password in bad_password):
+    if (password in password_blacklist):
         return 1
     if (len(password) < 8):
         return 2
@@ -38,7 +45,7 @@ def get_password_strength(password):
         return 7
     if (password in personal_information):
         return 8
-    if (not ('$' in password) or ('@' in password) or ('#' in password)):
+    if (if_not_simbol_in_password(password, ['$', '@', '#'])):
         return 9
     return 10
 
